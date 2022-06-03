5a7d4d397db12e5eb657f03bcff6f04ecbe15fd5
Linus Frost <arecius88@gmail.com>
2022-06-03 14:18:24 +0200

    Lite mer valmöjligheter

--- main.py
+++ main.py
@@ -2,21 +2,39 @@ import random
 from Modules import uke_wasa, atemi_wasa, kansetsu_wasa, nage_wasa, kihon, kihon_selecter
 
 start = False
+
+#Importerar och konverterar listorna från Modules.py
 uke_wasa = uke_wasa()
 atemi_wasa = atemi_wasa()
 kansetsu_wasa = kansetsu_wasa()
 nage_wasa = nage_wasa()
 kihon = kihon()
 
-
+#Loop för att starta valen
 while start != True:
-    choice = input("\nVälj teknikfamilj (Uke wasa, Atemi wasa, Kansetsu wasa, Nage wasa or all kihon):")
+    choice = input("\nVälj teknikfamilj (Uke wasa, Atemi wasa, Kansetsu wasa, Nage wasa or all kihon):").lower()
 
     if choice == "u":
         selected_group = kihon_selecter(uke_wasa)
-        
-        if len(selected_group) == 0:
-            start = True
+
+        #Försök till att lägga till extra val om jag vill fortsätta i samma teknikfamilj. 
+        choice2 = input("ny teknik j/n: ").lower() 
+        while choice2 == "j":
+            selected_group = kihon_selecter(uke_wasa)
+            choice2 = input("ny teknik j/n: ").lower()
+
+            if choice2 == "n":
+                break 
+
+#Avslutningskriterium.                
+            elif len(selected_group) == 0:
+                start = True
+            
+            #Behöver lösa denna Else statment - Hoppar tillbaka till första Whileloop
+            else:
+                print("Svara med j eller n")
+      
+
 
 
     elif choice == "a":
@@ -43,8 +61,8 @@ while start != True:
         if len(selected_group) == 0:
             start = True        
  
-    elif choice == "quit":
+    elif choice == "q":
         start = True
 
     else:
-        print("Ange teknikfamilj")
+        print("/n Ange teknikfamilj")
