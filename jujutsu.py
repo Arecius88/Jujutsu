e7f7465e80083abc06f2ddbe7df2b009fb85070c
Linus Frost <arecius88@gmail.com>
2022-06-10 21:55:07 +0200

    Start och GUI

--- jujutsu.py
+++ jujutsu.py
@@ -1,13 +1,14 @@
-from tkinter import Widget
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+
 
 class MainWidget(Widget):
     pass
 
+
 class JujutsuApp(App):
     pass
-
 
 
 JujutsuApp().run()
