9a43c8b2a5d25a9ce4e5b6bb38f6fe89a1abae49
Arecius88 <arecius88@gmail.com>
2022-06-26 10:58:57 +0200

    lyckats slå ihop listor

--- MOCK.py
+++ MOCK.py
@@ -1,29 +1,38 @@
-# Försöker att spitta uke_wasa per grad. Målet är att ha någopn form av lösning 
-# där jag lägger till nya tekniker i på de tekikerna från den före gående listan
 
-ukewasa_yellow = ["Ju morote jodan uke, mot svingslag",
+add_ukewasa_yellow = ["Ju morote jodan uke, mot svingslag",
                   "Jodan uchi uke, mot rakt slag",
                   "Gedan uchi uke, mot cirkulär spark",
                   "Gedan juji uke, mot knästöt och rak spark"]    
     
-ukewasa_organge = ["Morote jodan uke, mot svingslag",
+add_ukewasa_orange = ["Morote jodan uke, mot svingslag",
                    "Chudan soto uke, mot rakt slag",
                    "Gedan soto uke, mot rak spark"]
 
-ukewasa_green = ["Jodan soto uke, mot rakt slag",
+add_ukewasa_green = ["Jodan soto uke, mot rakt slag",
                 "San ren uke, mot cirkulär spark",]
 
-ukewasa_blue = [ "Chudan uchi uke, mot rakt slag"]
+add_ukewasa_blue = [ "Chudan uchi uke, mot rakt slag"]
 
-ukewasa_brown = []
+add_ukewasa_brown = []
 
 
-#yellow_belt = ukewasa_yellow
-orange_belt = ukewasa_organge.extend(ukewasa_yellow)
+uke_wasa_yellow = add_ukewasa_yellow
+uke_wasa_orange = add_ukewasa_yellow + add_ukewasa_orange 
+uke_wasa_green = add_ukewasa_yellow + add_ukewasa_orange + add_ukewasa_green
+uke_wasa_blue = add_ukewasa_yellow + add_ukewasa_orange + add_ukewasa_green + add_ukewasa_blue
+uke_wasa_brown = uke_wasa_blue
 
-print(ukewasa_organge)
 
 '''
+#Troligen den metoden jag anävnder för att slå ihop de olika list. 
+orange = ukewasa_yellow + ukewasa_organge
+print(len(ukewasa_yellow))
+print(len(orange))
+print(orange)
+#yellow_belt = ukewasa_yellow
+#orange_belt = ukewasa_organge.extend(ukewasa_yellow)
+
+#print(ukewasa_organge)
 #ukewasa_orange = ukewasa_yellow.extend(add_organge_ukewasa)
 print(ukewasa_yellow)
 orange = ukewasa_yellow.extend(ukewasa_organge)
