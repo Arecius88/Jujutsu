5ab3c0c0f6440c29ef56ae7b8fe080a3600e3e2e
Linus Frost <arecius88@gmail.com>
2022-07-27 23:20:24 +0200

    fått till första steget på att få Spinner att byta skärmar

--- jujutus_screens.py
+++ jujutus_screens.py
@@ -2,13 +2,39 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+
+#Sakpa en class som hanterar alla byten av skärmar
+
+''' class spinner_handler():
+		def on_spinner_select_grades
+			Ändra från Home till en grad
+			
+		def on_spinner_select_technique
+			val av teknik som föregående ver av appen. 
+		'''
+
+
 #Define our different screens
 class HomeScreen(Screen):
-	pass
+	def on_spinner_select_grades(self, spinner_value):
+		print("1. Spinner value " + spinner_value)
+		if spinner_value == "Home":
+			self.parent.current = "home"
 
+		if spinner_value == "Yellow":
+			self.parent.current = "yellow"
+
+		elif spinner_value == "Orange":
+			self.parent.current = "orange"
 
 class YellowScreen(Screen):
-	pass
+	def on_spinner_select_technique(self, spinner_value):
+		print("1. Spinner value " + spinner_value)
+		if spinner_value == "Home":
+			self.parent.current = "home"
+
+		elif spinner_value == "Uke":
+			self.ids.yellow_screen_label.text = "Kanske är vi på rätt väg?"
 
 class OrangeScreen(Screen):
 	pass
@@ -32,13 +58,14 @@ class ThirdDanScreen(Screen):
 	pass
 
 class WindowManager(ScreenManager):
-	def on_spinner_select(self, value):
-		print("Spinner value " + value)
+	pass
+
+
 
 
 sm = ScreenManager()
-
-
+sm.add_widget(HomeScreen(name="home"))
+sm.add_widget(YellowScreen(name = "yellow"))
 
 
 # Designate Our .kv design file
