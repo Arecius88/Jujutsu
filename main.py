5240efe8efe7c0a5414a42ac0a41cf633a86d9d8
Linus Frost <86301455+Arecius88@users.noreply.github.com>
2025-05-30 21:17:56 +0200

    Skapat en ny Homescreen

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 
 # Designate Our .kv design file
 #kv = Builder.load_file('jujutsu_screen.kv')
 
-class test_scren(Widget):
+class test_screen(Widget):
 	pass
 
 class jujutsu_screenApp(App):
 	def build(self):
-		return test_scren()
+		return test_screen()
 
 
 
